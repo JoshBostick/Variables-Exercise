fa67e3c81b71e9bf7f56dd02b84748bd46c4d7db
Josh Bostick <jbostick99@gmail.com>
2023-11-07 14:03:45 -0600

    Finished Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+string name = "Josh";
+int age = 43;
+char middleInitial = 'J';
+bool isOver18 = true;
+double currentTemp = 75;
+decimal currentPrice = 40.00m;
+
+Console.WriteLine($"Name: {name}\n" +
+                  $"Age: {age}\n" +
+                  $"Middle Initial: {middleInitial}\n" +
+                  $"Is Over 18?: {isOver18}\n" +
+                  $"Current Temp: {currentTemp}\n" +
+                  $"Current Price: {currentPrice}\n");
